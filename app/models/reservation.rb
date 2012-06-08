34a97c91c78939bd9f9891b7de8946c6d5c3bbfd
Christopher Thielen <cthielen@gmail.com>
2012-06-07 18:26:55 -0700

    Clean up reservation listing view

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,12 @@ class Reservation < ActiveRecord::Base
   attr_accessible :reason, :expiration, :requested_at, :status_id, :user_id, :reservation_type, :port
   belongs_to :status
   validates :user_id, :reason, :requested_at, :expiration, :status_id, :reservation_type, :presence => true
+  
+  def reservation_type_in_words
+    if reservation_type == 0
+      "Static IP"
+    else
+      "Firewall Exemption"
+    end
+  end
 end
