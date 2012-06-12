8c4afa500fd9d6ca2262d6a01c3260b74da278eb
Christopher Thielen <cthielen@gmail.com>
2012-06-12 15:37:26 -0700

    Sanity check the SysAid initializer

--- config/initializers/sysaid.rb
+++ config/initializers/sysaid.rb
@@ -1,9 +1,17 @@
 # Initializes SysAid based on settings in config/sysaid.yml
-sysaid_settings = YAML.load_file("#{Rails.root.to_s}/config/sysaid.yml")['sysaid']
+SYSAID_SUPPORT = false
 
-SysAid.server_settings = {
-  :endpoint => sysaid_settings['endpoint'],
-  :account => sysaid_settings['account'],
-  :username => sysaid_settings['username'],
-  :password => sysaid_settings['password']
-}
+begin
+  sysaid_settings = YAML.load_file("#{Rails.root.to_s}/config/sysaid.yml")['sysaid']
+  
+  SysAid.server_settings = {
+    :endpoint => sysaid_settings['endpoint'],
+    :account => sysaid_settings['account'],
+    :username => sysaid_settings['username'],
+    :password => sysaid_settings['password']
+  }
+  
+  SYSAID_SUPPORT = true
+rescue Errno::ENOENT => e
+  logger.warn "config/sysaid.yml is missing. Disabling SysAid support."
+end
