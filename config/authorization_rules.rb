8e768c874affcadfd5999b238af9995f52f57559
Christopher Thielen <cthielen@gmail.com>
2012-07-03 10:33:41 -0700

    Be sure to allow anonymous users to the welcome page

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -3,7 +3,10 @@ authorization do
   role :admin do
     has_permission_on :reservations, :to => [:create, :read, :update, :delete]
   end
-  role :user do
+  role :access do
     has_permission_on :reservations, :to => [:create, :read, :update, :delete]
+  end
+  role :guest do
+    has_permission_on :application, :to => [:welcome]
   end
 end
