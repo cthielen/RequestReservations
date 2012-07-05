ae5d6a26faacfd0a4c609cd27e7fa314ba7f9b73
Christopher Thielen <cthielen@gmail.com>
2012-07-05 11:36:18 -0700

    Updated authorization rules and added subnet dialog markup

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -4,6 +4,7 @@ authorization do
     has_permission_on :reservations, :to => [:new, :index, :show, :update, :delete]
     has_permission_on :application, :to => [:about]
     has_permission_on :admin_subnets, :to => [:index]
+    has_permission_on :subnets, :to => [:create]
   end
   role :access do
     has_permission_on :reservations, :to => [:new, :index, :show, :update, :delete]
