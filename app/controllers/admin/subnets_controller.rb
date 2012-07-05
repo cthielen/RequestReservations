cb22f834e474eff6d46a12538b0b4cf28b35153f
Christopher Thielen <cthielen@gmail.com>
2012-07-05 14:09:58 -0700

    Finished subnet dialog

--- app/controllers/admin/subnets_controller.rb
+++ app/controllers/admin/subnets_controller.rb
@@ -15,24 +15,20 @@ class Admin::SubnetsController < Admin::BaseController
     end
   end
 
-  # GET /subnets/1
   # GET /subnets/1.json
   def show
     @subnet = Subnet.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
       format.json { render json: @subnet }
     end
   end
 
-  # GET /subnets/new
   # GET /subnets/new.json
   def new
     @subnet = Subnet.new
 
     respond_to do |format|
-      format.html # new.html.erb
       format.json { render json: @subnet }
     end
   end
@@ -42,47 +38,38 @@ class Admin::SubnetsController < Admin::BaseController
     @subnet = Subnet.find(params[:id])
   end
 
-  # POST /subnets
   # POST /subnets.json
   def create
     @subnet = Subnet.new(params[:subnet])
 
     respond_to do |format|
       if @subnet.save
-        format.html { redirect_to admin_subnets_path, notice: 'Subnet was successfully created.' }
-        format.json { render json: @subnet, status: :created, location: @subnet }
+        format.json { render json: @subnet, status: :created, location: admin_subnet_path(@subnet) }
       else
-        format.html { render action: "new" }
         format.json { render json: @subnet.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PUT /subnets/1
   # PUT /subnets/1.json
   def update
     @subnet = Subnet.find(params[:id])
 
     respond_to do |format|
       if @subnet.update_attributes(params[:Subnet])
-        format.html { redirect_to @subnet, notice: 'Subnet was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
         format.json { render json: @subnet.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /subnets/1
   # DELETE /subnets/1.json
-  # Note: We never delete a Subnet. We merely change its status to cancelled.
   def destroy
     @subnet = Subnet.find(params[:id])
     @subnet.destroy
 
     respond_to do |format|
-      format.html { redirect_to admin_subnets_url }
       format.json { head :no_content }
     end
   end
--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -3,8 +3,8 @@ authorization do
   role :admin do
     has_permission_on :reservations, :to => [:new, :index, :show, :update, :delete]
     has_permission_on :application, :to => [:welcome, :about]
-    has_permission_on :admin_subnets, :to => [:index, :create]
-    has_permission_on :subnets, :to => [:create]
+    has_permission_on :admin_subnets, :to => [:index, :create, :destroy]
+    has_permission_on :subnets, :to => [:create, :delete]
   end
   role :access do
     has_permission_on :reservations, :to => [:new, :index, :show, :update, :delete]
