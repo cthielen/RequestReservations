2b75cdd0f6fc9f4bf1c284a4b499d5db7ba12c44
Christopher Thielen <cthielen@gmail.com>
2012-07-05 14:36:19 -0700

    Minor permissions update

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -2,13 +2,13 @@
 authorization do
   role :admin do
     has_permission_on :reservations, :to => [:new, :index, :show, :update, :delete, :create, :destroy]
-    has_permission_on :application, :to => [:welcome, :about]
+    has_permission_on :application, :to => [:welcome, :about, :logout]
     has_permission_on :admin_subnets, :to => [:index, :create, :destroy]
     has_permission_on :subnets, :to => [:create, :delete]
   end
   role :access do
     has_permission_on :reservations, :to => [:new, :index, :show, :update, :delete, :create, :destroy]
-    has_permission_on :application, :to => [:welcome]
+    has_permission_on :application, :to => [:welcome, :logout]
   end
   role :guest do
     has_permission_on :application, :to => [:welcome]
