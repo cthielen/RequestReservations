56c70db3cfbfabf0bd450ad5a4ec53e7ac49f9c6
Christopher Thielen <cthielen@gmail.com>
2012-07-17 13:11:03 -0700

    Augmented a previous migration to support authorization

--- db/migrate/20120612234047_add_cancelled_status_to_reservations.rb
+++ db/migrate/20120612234047_add_cancelled_status_to_reservations.rb
@@ -1,11 +1,13 @@
 class AddCancelledStatusToReservations < ActiveRecord::Migration
   def up
+    Authorization.ignore_access_control(true)
     s = Status.new
     s.label = "Cancelled"
     s.save
   end
-  
+
   def down
+    Authorization.ignore_access_control(true)
     s = Status.find_by_label("Cancelled")
     unless s.nil?
       s.delete
